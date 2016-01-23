2b2a9e5ef60a572e819d4bdb6d8328fd437bffc7
orbisnull <zen4dev@gmail.com>
2016-01-23 13:30:44 +0800

    fix timestamp in database

---
+++ src/PermAuth/migrations/20160118063432_perm_auth_date_to_timestamp_without_zone_pg.php
@@ -0,0 +1,38 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class PermAuthDateToTimestampWithoutZonePg extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $table = $this->table("perm_auth_series");
+        $table->changeColumn("created", "timestamp", ["timezone" => false, "default" => "CURRENT_TIMESTAMP"])
+            ->update();
+
+        $table = $this->table("perm_auth_tokens");
+        $table->changeColumn("created", "timestamp", ["timezone" => false, "default" => "CURRENT_TIMESTAMP"])
+            ->update();
+    }
+}
